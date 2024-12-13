00bcccf744ee36185a13d154a489fe79666de874
vanhhnguyen <vananh03910@gmail.com>
2024-12-13 21:40:42 +0700

    score-management-fe.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ const Footer = () => {
     <footer className="bg-primary text-white p-4 text-center">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-lg sm:text-xl font-bold mb-2">
-          PHÒNG ĐÀO TẠO ĐẠI HỌC
+          PHÒNG ĐÀO TẠO 
         </h2>
 
         <div className="space-y-1 text-sm sm:text-base">
-          <p>Phòng A120, Trường Đại học Công nghệ Thông tin.</p>
-          <p>Khu phố 6, P.Linh Trung, Tp.Thủ Đức, Tp.Hồ Chí Minh.</p>
+          <p>Trung Tâm Ngoại Ngữ IDP.</p>
+          <p>Khu phố 6, P.Tân Mai, Tp.Biên Hòa, Đồng Nai.</p>
           <p className="flex items-center justify-center flex-wrap gap-1">
-            <span>Điện thoại: (028) 372 51993,</span>
+            <span>Điện thoại: (028) 393 3939,</span>
             <span>
               Ext: <span className="text-yellow-400">113</span>(Hệ từ xa qua
               mạng), <span className="text-yellow-400">112</span>(Hệ chính quy).
@@ -19,10 +19,10 @@ const Footer = () => {
           <p>
             Email:{" "}
             <a
-              href="mailto:phongdaotaodh@uit.edu.vn"
+              href="mailto:phongdaotao@IDP.edu.vn"
               className="text-blue-400 hover:underline"
             >
-              phongdaotaodh@uit.edu.vn
+              phongdaotao@IDP.edu.vn
             </a>
           </p>
         </div>
